d451f858e6d4ba3a082baac3443f5d2ae69b399b
Isaque4nd <andradeisq1.7@gmail.com>
2025-08-25 00:51:48 -0300

    +

--- upload_this/routes/user.js
+++ upload_this/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('upload_this\\database\\dbpromise.js');
 const { query } = require("../database/dbpromise.js");
 const randomstring = require("randomstring");
 const bcrypt = require("bcrypt");
