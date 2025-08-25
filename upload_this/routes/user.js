5f8c2e8304858af232b2962a7330ece5002eb659
Isaque4nd <andradeisq1.7@gmail.com>
2025-08-25 00:37:31 -0300

    +

--- upload_this/routes/user.js
+++ upload_this/routes/user.js
@@ -1,9 +1,8 @@
-
+const express = require("express");
+const router = express.Router();
 const { query } = require("../database/dbpromise.js");
 const randomstring = require("randomstring");
 const bcrypt = require("bcrypt");
-const router = express.Router();
-const pool = require('../db');
 const {
   isValidEmail,
   getFileExtension,
@@ -2656,14 +2655,8 @@ router.post("/send_template_message", validateUser, async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erro no servidor');
-  }
-});
-
 module.exports = router;
+
+const express = require("express");
+
+
